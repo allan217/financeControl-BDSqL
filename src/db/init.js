adc8824b2bc1ad749d64d977509bf539ad914e9b
Allan Carlos <sanguec777@gmail.com>
2024-05-16 16:54:29 -0300

    at

--- src/db/init.js
+++ src/db/init.js
@@ -1,23 +1,24 @@
-const Database = require('./config');
+const connection = require('./config');
 
 const initDB = {
     
     async init() {
+        let db;
         try {
-            const db = await Database()
+            db = await connection.connect();
 
             await db.query(`CREATE TABLE IF NOT EXISTS profile (
                 id INTEGER PRIMARY KEY AUTO_INCREMENT,
-                name TEXT,
-                avatar TEXT,
-                cpf TEXT
+                name VARCHAR(255),
+                avatar VARCHAR(255),
+                cpf VARCHAR(14)
             )`);
 
             await db.query(`CREATE TABLE dadosLogin (
                 id INTEGER PRIMARY KEY AUTO_INCREMENT,
-                name TEXT,
-                email TEXT,
-                password TEXT
+                name VARCHAR(255),
+                email VARCHAR(255),
+                password VARCHAR(255)
             )`);
 
             await db.query(`CREATE TABLE trans (
@@ -25,15 +26,17 @@ const initDB = {
                 description TEXT,
                 amount FLOAT,
                 date DATETIME
-            )`)
+            )`);
     
     
-            await db.end()
-            console.log("Dados inseridos com sucesso!!")
+            
+            console.log("Tabelas criadas com sucesso!!")
 
         } catch (error) {
-        console.log("Erro ao inserir dados no banco.", error.message)
+            console.log("Erro ao criar tabela no banco.", error.message)
     
+        } finally {
+            if (db) await db.end();
         }
     }
 }
