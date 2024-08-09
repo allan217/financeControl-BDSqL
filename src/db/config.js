f15f4e3366c90639a48b2a650c06b23c0a9f3c9e
Allan Carlos <73024275+allan217@users.noreply.github.com>
2024-08-09 19:26:51 -0300

    Update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '7}-+dGV#f|',
-    database: 'databaseincontrol'
+    host: 'databasemysql.c9yuamgqklwd.sa-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: '7}-+dGV#f|allanCS217',
+    database: 'databaseincontrolAWS'
 })
 
 connection.connect((err) => {
